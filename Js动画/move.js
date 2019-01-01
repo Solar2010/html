da1506758b90348d26873da5998af40a66b6eeef
Ian <786901955@qq.com>
2019-01-01 23:10:24 +0800

    case animation

--- Js动画/move.js
+++ Js动画/move.js
@@ -6,30 +6,35 @@ function getStyle(obj,attr){
 	}
 }
 
-function startMove(obj,attr,iTarget,fn) {
+function startMove(obj,json,fn) {
+	var flag = true;//开关
 	clearInterval(obj.timer);
 	obj.timer = setInterval(function(){
 		var icur = 0;
-		if(attr == 'opacity') {
-			icur = Math.round(parseFloat(getStyle(obj,attr))*100);
-		} else {
-			icur = parseInt(getStyle(obj,attr));
-		}
-
-		var speed = (iTarget - icur) / 8;
-		speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-
-		if(icur == iTarget) {
-			clearInterval(obj.timer);
-			if(fn) {
-				fn();
-			}
-		} else {
+		for(attr in json) {
 			if(attr == 'opacity') {
-				obj.style.filter = 'alpha:(opacity:' + icur + speed + ')';
-				obj.style.opacity = (icur + speed)/100;
+				icur = Math.round(parseFloat(getStyle(obj,attr))*100);
 			} else {
-				obj.style[attr] = parseInt(getStyle(obj,attr)) + speed + 'px';
+				icur = parseInt(getStyle(obj,attr));
+			}
+
+			var speed = (json[attr] - icur) / 8;
+			speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+
+			if(icur != json[attr]) {
+				flag = false;
+				if(attr == 'opacity') {
+					obj.style.filter = 'alpha:(opacity:' + icur + speed + ')';
+					obj.style.opacity = (icur + speed)/100;
+				} else {
+					obj.style[attr] = parseInt(getStyle(obj,attr)) + speed + 'px';
+				}
+			} 
+			if(flag) {
+				clearInterval(obj.timer);
+				if(fn) {
+					fn();
+				}
 			}
 		}
 	},30);
